c6739709814c6c903202cc7b1eb6ce7c39f412b2
matt-carmo <matheus2018i@gmail.com>
2024-10-02 08:07:15 -0300

    first-commit fix Footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 'use client'
 import Link from "next/link";
-import React, { Context } from "react";
+import React  from "react";
 
 interface iFooter extends React.HTMLAttributes<HTMLElement> {
     className?: string;
